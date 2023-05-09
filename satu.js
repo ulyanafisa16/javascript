062cf25e48a455926b80caafac1b194579ae694e
Ulya Nafisatussaniyah <ulyanafisa0@gmail.com>
2023-05-09 10:26:49 +0700

    satu

--- satu.js
+++ satu.js
@@ -25,21 +25,4 @@ signupBtn.classList.remove("disable");
 signinBtn.classList.add("disable");
 }
 
-// Ambil elemen input password
-var passwordInput = document.getElementById('passwordd');
-
-// Tambahkan event listener untuk menangkap input dari pengguna
-passwordInput.addEventListener('input', function() {
-  // Ambil nilai password yang dimasukkan pengguna
-  var passwordValue = passwordInput.value;
-  
-  // Periksa apakah panjang password lebih dari 8 karakter
-  if (passwordValue.length < 8) {
-    // Jika kurang dari 8 karakter, tampilkan pesan error
-    passwordInput.setCustomValidity('Password harus memiliki setidaknya 8 karakter');
-  } else {
-    // Jika sudah mencapai 8 karakter atau lebih, hapus pesan error
-    passwordInput.setCustomValidity('');
-  }
-});
 
